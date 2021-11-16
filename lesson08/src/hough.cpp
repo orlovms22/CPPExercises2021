80ea0770c090f5076361f7506a9d4565880c49b0
orlovms22 <misha06112004@yandex.ru>
2021-11-16 22:40:11 +0300

    hough_extremums

--- lesson08/src/hough.cpp
+++ lesson08/src/hough.cpp
@@ -1,12 +1,10 @@
 #include "hough.h"
 
 #include <libutils/rasserts.h>
+#include <float.h>
 
-double toRadians(double degrees)
-{
-    const double PI = 3.14159265358979323846264338327950288;
-    return degrees * PI / 180.0;
-}
+#define PI 3.14159265358979323846264338327950288
+#define DEG_TO_RAD(x) (x) * PI / 180
 
 double estimateR(double x0, double y0, double theta0radians)
 {
@@ -18,10 +16,6 @@ cv::Mat buildHough(cv::Mat sobel) {// единственный аргумент 
     // проверяем что входная картинка - одноканальная и вещественная:
     rassert(sobel.type() == CV_32FC1, 237128273918006);
 
-    // TODO
-    // Эта функция по картинке с силами градиентов (после свертки оператором Собеля) строит пространство Хафа
-    // Вы можете либо взять свою реализацию из прошлого задания, либо взять эту заготовку:
-
     int width = sobel.cols;
     int height = sobel.rows;
 
@@ -29,42 +23,63 @@ cv::Mat buildHough(cv::Mat sobel) {// единственный аргумент 
     int max_theta = 360;
 
     // решаем какое максимальное значение у параметра r в нашем пространстве прямых:
-    int max_r = width + height;
+    int max_r = sqrt(width * width + height * height) + 1; // TODO замените это число так как вам кажется правильным (отталкиваясь от разрешения картинки - ее ширины и высоты)
 
     // создаем картинку-аккумулятор, в которой мы будем накапливать суммарные голоса за прямые
     // так же известна как пространство Хафа
-    cv::Mat accumulator(max_r, max_theta, CV_32FC1, 0.0f); // зануление аккумулятора через указание значения по умолчанию в конструкторе
+    cv::Mat accumulator(max_r, max_theta, CV_32FC1, 0.0f); // TODO подумайте какого разрешения она должна быть и поправьте ее размер
+    // TODO не забудьте заполнить эту матрицу-картинку-аккумулятор нулями (очистить)
 
     // проходим по всем пикселям нашей картинки (уже свернутой оператором Собеля)
-    for (int y0 = 0; y0 < height; ++y0) {
-        for (int x0 = 0; x0 < width; ++x0) {
+    for (int x0 = 0; x0 < height; x0++) {
+        for (int y0 = 0; y0 < width; y0++) {
             // смотрим на пиксель с координатами (x0, y0)
-            float strength = sobel.at<float>(y0, x0);
+            float strength = sobel.at<float>(x0, y0) / FLT_MAX;// TODO считайте его "силу градиента" из картинки sobel
 
             // теперь для текущего пикселя надо найти все возможные прямые которые через него проходят
             // переберем параметр theta по всему возможному диапазону (в градусах):
-            for (int theta0 = 0; theta0 + 1 < max_theta; ++theta0) {
+            for (int theta0 = 0; theta0 < max_theta - 1; theta0++) {
+                // TODO рассчитайте на базе информации о том какие координаты у пикселя - (x0, y0) и какой параметр theta0 мы сейчас рассматриваем
+                // TODO обратите внимание что функции sin/cos принимают углы в радианах, поэтому сначала нужно пересчитать theta0 в радианы (воспользуйтесь константой PI)
+                float r0 = x0 * cos(DEG_TO_RAD(theta0)) + y0 * sin(DEG_TO_RAD(theta0));
+                int theta1 = theta0 + 1;
+                float r1 = x0 * cos(DEG_TO_RAD(theta1)) + y0 * sin(DEG_TO_RAD(theta1));
 
-                double theta0radians = toRadians(theta0);
-                int r0 = (int) round(estimateR(x0, y0, theta0radians)); // оцениваем r0 и округляем его до целого числа
-                if (r0 < 0 || r0 >= max_r)
-                    continue;
+                float fromR = std::min(r0, r1);
+                float toR = std::max(r0, r1);
 
-                // TODO надо определить в какие пиксели i,j надо внести наш голос с учетом проблемы "Почему два экстремума?" обозначенной на странице:
-                // https://www.polarnick.com/blogs/239/2021/school239_11_2021_2022/2021/11/09/lesson9-hough2-interpolation-extremum-detection.html
+                // TODO теперь рассчитайте координаты пикслея в пространстве Хафа (в картинке-аккумуляторе) соответсвующего параметрам theta0, r0
 
-//                        // чтобы проверить не вышли ли мы за пределы картинки-аккумулятора - давайте явно это проверим
-//                        rassert(i >= 0, 237891731289044);
-//                        rassert(i < accumulator.cols, 237891731289045);
-//                        rassert(j >= 0, 237891731289046);
-//                        rassert(j < accumulator.rows, 237891731289047);
-//                        // теперь легко отладить случай выхода за пределы картинки
-//                        // просто поставьте точку остановки внутри rassert:
-//                        // нажмите Ctrl+Shift+N -> rasserts.cpp
-//                        // и поставьте точку остановки на 8 строке: "return line;"
-//
-//                        // и добоавляем в картинку-аккумулятор наш голос с весом strength (взятый из картинки свернутой Собелем)
-//                        accumulator.at<float>(j, i) += strength;
+                if (fromR > 0 && toR > 0) {
+                    // чтобы проверить не вышли ли мы за пределы картинки-аккумулятора - давайте явно это проверим:
+                    rassert(fromR < accumulator.rows, 237891731289044);
+                    rassert(toR < accumulator.rows, 237891731289045);
+                    rassert(theta0 >= 0, 237891731289046);
+                    rassert(theta0 + 1 < accumulator.cols, 237891731289047);
+                    // теперь легко отладить случай выхода за пределы картинки
+                    // TODO просто поставьте точку остановки внутри rassert:
+                    // нажмите Ctrl+Shift+N -> rasserts.cpp
+                    // и поставьте точку остановки на 8 строке: "return line;"
+
+                    // TODO и добавьте в картинку-аккумулятор наш голос с весом strength (взятый из картинки свернутой Собелем)
+                    //accumulator.at<float>(r0, theta0) += strength;
+                    for (int r = fromR; r < toR; r++)
+                    {
+                        accumulator.at<float>(r, theta0) += strength;
+                        accumulator.at<float>(r, theta1) += strength;
+                        /*float k = (r - fromR) / (toR - fromR);
+                        if (r0 >= r1)
+                        {
+                            accumulator.at<float>(r, theta0) += strength * k;
+                            accumulator.at<float>(r, theta1) += strength * (1 - k);
+                        }
+                        else
+                        {
+                            accumulator.at<float>(r, theta0) += strength * (1 - k);
+                            accumulator.at<float>(r, theta1) += strength * k;
+                        }*/
+                    }
+                }
             }
         }
     }
@@ -80,16 +95,27 @@ std::vector<PolarLineExtremum> findLocalExtremums(cv::Mat houghSpace)
     rassert(houghSpace.cols == max_theta, 233892742893082);
     const int max_r = houghSpace.rows;
 
+    const int rad = 1;
+
     std::vector<PolarLineExtremum> winners;
 
-    for (int theta = 0; theta < max_theta; ++theta) {
-        for (int r = 0; r < max_r; ++r) {
-            // TODO
-            // ...
-            // if (ok) {
-            //     PolarLineExtremum line(theta, r, votes);
-            //     winners.push_back(line);
-            // }
+    for (int theta = rad; theta < max_theta - rad; theta++) {
+        for (int r = rad; r < max_r - rad; r++) {
+            bool flag = true;
+            for (int i = -rad; i < rad; i++)
+            {
+                for (int j = -rad; j < rad; j++)
+                {
+                    if (houghSpace.at<float>(r + i, theta + j) > houghSpace.at<float>(r, theta))
+                    {
+                        flag = false;
+                    }
+                }
+            }
+             if (flag) {
+                 PolarLineExtremum line(theta, r, houghSpace.at<float>(r, theta));
+                 winners.push_back(line);
+             }
         }
     }
 
@@ -100,10 +126,19 @@ std::vector<PolarLineExtremum> filterStrongLines(std::vector<PolarLineExtremum> 
 {
     std::vector<PolarLineExtremum> strongLines;
 
-    // Эта функция по множеству всех найденных локальных экстремумов (прямых) находит самую популярную прямую
-    // и возвращает только вектор из тех прямых, что не сильно ее хуже (набрали хотя бы thresholdFromWinner голосов от победителя, т.е. например половину)
-
-    // TODO
+    double max_votes = 0;
+    for (int i = 0; i < allLines.size(); i++)
+    {
+        max_votes = std::max(max_votes, allLines[i].votes);
+    }
+    for (int i = 0; i < allLines.size(); i++)
+    {
+        if (allLines[i].votes > max_votes * thresholdFromWinner)
+        {
+            PolarLineExtremum line(allLines[i].theta, allLines[i].r, allLines[i].votes);
+            strongLines.push_back(line);
+        }
+    }
 
     return strongLines;
 }
--- lesson08/src/main.cpp
+++ lesson08/src/main.cpp
@@ -10,9 +10,9 @@
 void test(std::string name) {
     std::cout << "Processing image " << name << ".jpg..." << std::endl;
 
-    std::string full_path = "lesson05/data/" + name + ".jpg";
+    std::string full_path = "../../../../lesson05/data/" + name + ".jpg";
     cv::Mat img = cv::imread(full_path);
-    cv::imwrite("lesson08/resultsData/" + name + "_0.png", img); // сохраним еще раз изначальную картинку в папку с результатами чтобы была под рукой
+    cv::imwrite("../../../../lesson08/resultsData/" + name + "_0.png", img); // сохраним еще раз изначальную картинку в папку с результатами чтобы была под рукой
     rassert(!img.empty(), 238982391080010);
     cv::cvtColor(img, img, cv::COLOR_BGR2GRAY); // преобразуем в оттенки серого
 
@@ -30,15 +30,15 @@ void test(std::string name) {
 
     cv::Mat sobel_strength(img.rows, img.cols, CV_32FC1, 0.0f);
     // теперь хочется заполнить sobel_strength силой градиента с учетом обеих осей, т.е. что-то вроде sobel_strength=sqrt(grad_x^2+grad_x^2):
-    for (int j = 0; j < sobel_strength.rows; ++j) {
-        for (int i = 0; i < sobel_strength.cols; ++i) {
-            float dx = grad_x.at<float>(j, i);
-            float dy = grad_y.at<float>(j, i);
+    for (int i = 0; i < sobel_strength.rows; i++) {
+        for (int j = 0; j < sobel_strength.cols; j++) {
+            float dx = grad_x.at<float>(i, j);
+            float dy = grad_y.at<float>(i, j);
             float gradient_strength = sqrtf(dx*dx+dy*dy);
-            sobel_strength.at<float>(j, i) = gradient_strength;
+            sobel_strength.at<float>(i, j) = gradient_strength;
         }
     }
-    cv::imwrite("lesson08/resultsData/" + name + "_1_sobel_strength.png", sobel_strength);
+    cv::imwrite("../../../../lesson08/resultsData/" + name + "_1_sobel_strength.png", sobel_strength);
 
     // TODO
     // Эта функция по картинке с силами градиентов (после свертки оператором Собеля) строит пространство Хафа
@@ -49,14 +49,14 @@ void test(std::string name) {
     // но яркость иногда сильно больше чем 255
     // поэтому найдем максимальную яркость (max_accumulated) среди всей матрицы hough и после этого отнормируем всю картинку:
     float max_accumulated = 0.0f;
-    for (int j = 0; j < hough.rows; ++j) {
-        for (int i = 0; i < hough.cols; ++i) {
-            max_accumulated = std::max(max_accumulated, hough.at<float>(j, i));
+    for (int i = 0; i < hough.rows; i++) {
+        for (int j = 0; j < hough.cols; j++) {
+            max_accumulated = std::max(max_accumulated, hough.at<float>(i, j));
         }
     }
 
     // заменим каждый пиксель с яркости X на яркость X*255.0f/max_accumulated (т.е. уменьшим диапазон значений)
-    cv::imwrite("lesson08/resultsData/" + name + "_2_hough_normalized.png", hough*255.0f/max_accumulated);
+    cv::imwrite("../../../../lesson08/resultsData/" + name + "_2_hough_normalized.png", hough*255.0f/max_accumulated);
 
 // TODO здесь может быть полезно сгладить пространство Хафа, см. комментарии на сайте - https://www.polarnick.com/blogs/239/2021/school239_11_2021_2022/2021/11/09/lesson9-hough2-interpolation-extremum-detection.html
 
@@ -64,8 +64,8 @@ void test(std::string name) {
     std::vector<PolarLineExtremum> lines = findLocalExtremums(hough);
 
 // TODO реализуйте фильтрацию прямых - нужно оставлять только те прямые, у кого много голосов (реализуйте функцию filterStrongLines(...) ):
-//    double thresholdFromWinner = 0.5; // хотим оставить только те прямые у кого не менее половины голосов по сравнению с самой популярной прямой
-//    lines = filterStrongLines(lines, thresholdFromWinner);
+    double thresholdFromWinner = 0.8; // хотим оставить только те прямые у кого не менее половины голосов по сравнению с самой популярной прямой
+    lines = filterStrongLines(lines, thresholdFromWinner);
 
     std::cout << "Found " << lines.size() << " extremums:" << std::endl;
     for (int i = 0; i < lines.size(); ++i) {
@@ -78,19 +78,19 @@ int main() {
     try {
         test("line01");
 
-//        test("line02");
+        test("line02");
 
-//        test("line11");
+        test("line11");
 
-//        test("line12");
+        test("line12");
 
-//        test("line21_water_horizont");
+        test("line21_water_horizont");
 
-//        test("multiline1_paper_on_table");
+        test("multiline1_paper_on_table");
 
-//        test("multiline2_paper_on_table");
+        test("multiline2_paper_on_table");
 
-//        test("valve");
+        test("valve");
 
         return 0;
     } catch (const std::exception &e) {
